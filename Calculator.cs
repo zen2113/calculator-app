20e058611dacd93c175d7f7f91e93cb340c1f205
zezenn <zeinmustofa62@gmail.com>
2025-03-13 10:10:19 +0700

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -2,15 +2,12 @@ class Calculator
 {
 	static void Main(string[] args)
 	{
-		Console.Title = "Aplikasi Calculator";
 
-            int a = 10;
-            int b = 6;
+            int a = 10;// declarasi variabel a,dg nilsi awal 10;
+            int b = 6;// declarasi variabel b,dg nilsi awal 6;
 
             Console.WriteLine("Hasil Penambahan " + a + " + " + b + " = " + Penambahan(a, b));
             Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
-            Console.WriteLine("Hasil Perkalian {0} * {1} = {2}", a, b, Perkalian(a, b));
-            Console.WriteLine("Hasil Pembagian {0} / {1} = {2}", a, b, Pembagian(a, b));
 
             Console.WriteLine("\nTekan sembarang key untuk keluar");
             Console.ReadKey();
@@ -26,13 +23,4 @@ class Calculator
             return a - b;
         }
 		
-		static int Perkalian (int a, int b)
-        {
-            return a * b;
-        }
-
-        static float Pembagian (float a, float b)
-        {
-            return a / b;
-        }
 }
