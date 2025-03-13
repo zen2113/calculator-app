e6766c81ea286d184679a73fce3240c4bff75907
zen2113 <zeinmustofa6181@students.amikom.ac.id>
2025-03-13 09:47:16 +0700

    nini praktikum kedua

---
+++ Calculator.cs
@@ -0,0 +1,38 @@
+class Calculator
+{
+	static void Main(string[] args)
+	{
+		Console.Title = "Aplikasi Calculator";
+
+            int a = 10;
+            int b = 6;
+
+            Console.WriteLine("Hasil Penambahan " + a + " + " + b + " = " + Penambahan(a, b));
+            Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}", a, b, Pengurangan(a, b));
+            Console.WriteLine("Hasil Perkalian {0} * {1} = {2}", a, b, Perkalian(a, b));
+            Console.WriteLine("Hasil Pembagian {0} / {1} = {2}", a, b, Pembagian(a, b));
+
+            Console.WriteLine("\nTekan sembarang key untuk keluar");
+            Console.ReadKey();
+	}
+	
+	static int Penambahan (int a, int b)
+        {
+            return a + b;
+        }
+		
+		static int Pengurangan (int a, int b)
+        {
+            return a - b;
+        }
+		
+		static int Perkalian (int a, int b)
+        {
+            return a * b;
+        }
+
+        static float Pembagian (float a, float b)
+        {
+            return a / b;
+        }
+}
